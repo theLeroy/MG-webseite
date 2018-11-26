47051c99ee691bfe28a85b0be755c49452226991
Leroy <4c65726f79@gmail.com>
2018-11-26 18:39:39 +0100

    added acts

--- src/views/Timeline.vue
+++ src/views/Timeline.vue
@@ -53,70 +53,70 @@ export default {
       day: this.$route.query.day,
       Acts: [
         {
-          Time: '16:10',
+          Time: '16:30',
           Name: 'Alt-j',
           Genre: 'Alternative Rock',
           Day: '1',
           Description: 'Die Innovative Alt-Rock Band aus den Staaten bringt alternative Vibes!'
         },
         {
-          Time: '16:10',
+          Time: '17:30',
           Name: 'Bilderbuch',
           Genre: 'Indie Pop',
           Day: '1',
           Description: 'Popiger Crazy Shit aus Österreich'
         },
         {
-          Time: '16:10',
+          Time: '18:30',
           Name: 'Von Wegen Lisbeth',
           Genre: 'Indie',
           Day: '1',
           Description: 'Deepe Texte und erfrische Melodien'
         },
         {
-          Time: '16:10',
+          Time: '19:30',
           Name: 'Moop Mama',
           Genre: 'Mixed Styles',
           Day: '1',
           Description: 'Ein komplett anderer Mix aus diversen Musikformen ergibt eine Gefühlsachterbahn!'
         },
         {
-          Time: '16:10',
+          Time: '20:30',
           Name: 'Mine',
           Genre: 'Indie Folk',
           Day: '1',
           Description: 'Eine elegante Stimme revolutioniert den deutschen Pop'
         },
         {
-          Time: '16:10',
+          Time: '21:30',
           Name: 'Fatony',
           Genre: 'Rap',
           Day: '1',
           Description: 'Genialer Rapper mit stil'
         },
         {
-          Time: '16:10',
+          Time: '22:30',
           Name: 'AnnenMayKantereit',
           Genre: 'Indie Rock',
           Day: '1',
           Description: 'Die tiefste stimme in Europa'
         },
         {
-          Time: '16:10',
+          Time: '23:30',
           Name: 'Faber',
           Genre: 'Indie Rock',
           Day: '1',
           Description: 'Faber erweckt die Toten zum Leben und die Lebenden tötet er mit seiner Stimme und den atemberaubenden Texten'
         },
         {
-          Time: '16:10',
+          Time: '00:30',
           Name: 'Odezenne',
           Genre: 'Techno, Rap',
           Day: '1',
           Description: 'Ein neuer wind im Technogenre'
         },
         {
-          Time: '16:10',
+          Time: '01:30',
           Name: 'Jeans For Jesus',
           Genre: 'Indie Pop',
           Day: '1',
@@ -124,74 +124,144 @@ export default {
         },
 
         {
-          Time: '16:10',
+          Time: '16:30',
           Name: 'Leyya',
           Genre: 'Indie Pop',
           Day: '2',
           Description: 'Junge leute mit Stil und guter Musik'
         },
         {
-          Time: '16:10',
+          Time: '17:30',
           Name: 'Bonaparte',
           Genre: 'Visual Trash Punk',
           Day: '2',
           Description: 'Bonaparte ist nur schwer mit schwarzer Tinte festhaltbar'
         },
         {
-          Time: '16:10',
+          Time: '18:30',
           Name: 'Audio 88 & Yassin',
           Genre: 'HipHop',
-          Day: '3',
+          Day: '2',
           Description: 'Sozialkritische Texte vor das Volk!'
         },
         {
-          Time: '16:10',
+          Time: '19:30',
           Name: 'Kalabrese',
           Genre: 'Techno',
-          Day: '3',
+          Day: '2',
           Description: 'Techno im Züridialekt'
         },
         {
-          Time: '16:10',
+          Time: '20:30',
           Name: 'Glass Animals',
           Genre: 'Indie Rock',
-          Day: '3',
+          Day: '2',
           Description: 'Diese Musik macht mich Glücklich, dich auch?'
         },
         {
-          Time: '16:10',
+          Time: '21:30',
           Name: 'Gurr',
           Genre: 'Slacker Pop',
-          Day: '3',
+          Day: '2',
           Description: 'Wo Pop mit Rock verschwimmt'
         },
         {
-          Time: '16:10',
+          Time: '22:30',
           Name: 'Giant Rocks',
           Genre: 'Indie Rock',
-          Day: '3',
+          Day: '2',
           Description: 'Nicht nur der dame "Rockt"'
         },
         {
-          Time: '16:10',
+          Time: '23:30',
           Name: 'Her',
           Genre: 'Indie',
-          Day: '3',
+          Day: '2',
           Description: 'Die Indie-Superstars unserer Zeit'
         },
         {
-          Time: '16:10',
+          Time: '00:30',
           Name: 'The xx',
           Genre: 'Indie Rock',
-          Day: '3',
+          Day: '2',
           Description: 'Rocken das Indie Genre wie niemand sonst'
         },
         {
-          Time: '16:10',
+          Time: '01:30',
           Name: 'Irié Révoltés',
           Genre: 'Rap',
-          Day: '3',
+          Day: '2',
           Description: 'Bilingualer Rap, sozialkritisch mit Reggea gewürzt'
+        },
+        {
+          Time: '16:30',
+          Name: 'Sophie Hunger',
+          Genre: 'Rap',
+          Day: '3',
+          Description: 'Schweizer Musik vom feinsten'
+        },
+        {
+          Time: '17:30',
+          Name: 'Herr von Grau',
+          Genre: 'Rap',
+          Day: '3',
+          Description: 'Deutscher "Troll" rab der es in sich hat.'
+        },
+        {
+          Time: '18:30',
+          Name: 'Die Höchste Eisenbahn',
+          Genre: 'Rap',
+          Day: '3',
+          Description: 'Die besten depressiven gute Laune Musiker der Welt'
+        },
+        {
+          Time: '19:30',
+          Name: 'OK KID',
+          Genre: 'Rap',
+          Day: '3',
+          Description: 'Unverfangener Deutsch Rapp'
+        },
+        {
+          Time: '20:30',
+          Name: 'Rakede',
+          Genre: 'Rap',
+          Day: '3',
+          Description: 'Reggae so wie man ihn kennt und liebt.'
+        },
+        {
+          Time: '21:30',
+          Name: 'Tristan Busch',
+          Genre: 'Rap',
+          Day: '3',
+          Description: 'Der Text kommt mehrheitlich vom Julian. Bitte haben Sie Nachsicht.'
+        },
+        {
+          Time: '22:30',
+          Name: 'Kraftklub',
+          Genre: 'Rap',
+          Day: '3',
+          Description: 'Rock kann auch anders. Das beweist Kraftklub sehr gut.'
+        },
+        {
+          Time: '23:30',
+          Name: 'Beginner',
+          Genre: 'Rap',
+          Day: '3',
+          Description: 'Hiphop lebt. Das beweist Beginner bei jedem Auftrit neu.'
+        },
+        {
+          Time: '00:30',
+          Name: 'Mura Masa',
+          Genre: 'Rap',
+          Day: '3',
+          Description: 'Ein Internationaler Superstar ist auch vertreten.'
+        },
+        {
+          Time: '01:30',
+          Name: 'Childish Cambino',
+          Genre: 'Rap',
+          Day: '3',
+          Description: 'Der Abschluss ist ein unglaubliches Highlite und kommt mit Childish Cambino nicht zu kurz.'
         }
       ]
     }
