74c69546147133766402a4117c03677e4d731f22
Leroy <4c65726f79@gmail.com>
2019-01-08 14:19:44 +0100

    Finish without mobile

--- src/views/SocialWall.vue
+++ src/views/SocialWall.vue
@@ -16,7 +16,6 @@
             <button class="btn">Foto Hochladen</button>
             <input ref="Upload" id="Upload" type="file" name="Imgae" value="Upload" v-on:change="onFileChange">
           </div>
-          <!-- <input id="Submit" v-bind:class="{ show: show}" class="btn" type="submit" name="" value="Absenden"> -->
         </form>
       </div>
     </section>
@@ -24,6 +23,11 @@
       <SlideShow :url="url"/>
     </section>
     <section class="section" id="section3">
+      <iframe src="https://www.youtube-nocookie.com/embed/odsRJcaGMww" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe src="https://www.youtube-nocookie.com/embed/bfVK9z7BlUM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </section>
+    <section class="section" id="section4">
+      Bitte beachte, dass die Bilder, die du hier hochlädst für die Öffentlichkeit offen zugänglich sind. Dabei ist es wichtig, dass du die Einverständnis der Leute auf dem Bild hast.
     </section>
   </div>
 </template>
@@ -43,12 +47,23 @@ export default {
     return {
       url: [
         'https://c1.staticflickr.com/8/7172/6661760949_c089df9f5b_b.jpg',
+        'https://www.rollingstone.de/wp-content/uploads/2017/07/03/12/gettyimages-460475018-1.jpg',
         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQrCKJEtXXypSohOQdnz99U1wQ0PvWjeBQUg9QLkX6sEvHEn4bjQ',
         'http://www.lvz.de/var/storage/images/lvz/kultur/kultur-news/neues-rock-und-indie-festival-startet-diesen-sommer-in-ferropolis/198016043-1-ger-DE/Neues-Rock-und-Indie-Festival-startet-diesen-Sommer-in-Ferropolis_big_teaser_article.jpg',
         'https://oliknights.files.wordpress.com/2017/02/honestlondonurbanexploringlamarrgoldingparkour.jpg?w=660',
+        'https://i.ytimg.com/vi/pgApXCIlrGs/maxresdefault.jpg',
+        'https://apps-cloud.n-tv.de/img/19891182-1497512070000/16-9/1136/Alt-J-3.jpg',
+        'https://i1.wp.com/lh3.googleusercontent.com/-nc4Rk_zs28c/VtoIAUGkpbI/AAAAAAAAAYo/vBGQuA9Bl9c/s720-Ic42/leyya1.jpg?resize=350%2C200&ssl=1',
         'https://www.spreadshirt.de/blog/files/2015/05/blog_festival_Main.jpg',
         'https://www.hardnews.nl/wp-content/uploads/2015/06/Festival-camping-checklist.png',
         'http://www.simonchristen.com/images/albums/ue_001.jpg',
+        'https://www.be-subjective.de/wp-content/uploads/2018/10/20180926_TheRasmus_22_bs_KristinHofmann.jpg',
+        'https://www.thenational.ae/image/policy:1.698724:1516888082/wk26-music-wasla-mashrou-leila.jpg?f=16x9&w=1200&$p$f$w=03c683f',
+        'https://www.iamsterdam.com/media/agenda/festivals/amsterdam-open-air-2015-7amsterdam-open-air-2015.jpg',
+        'https://www.pangea-festival.de/files/content/Galeries/2018/Top%20Shots/%C2%A9NiclasRuehl_Pangea2018_lowres-907.jpg',
+        'https://thefusionfestival.co.uk/wp-content/uploads/2018/09/38-David-Guetta-Fusion-Charlotte_Wellings.jpg',
+        'https://style-magazin.ch/sites/default/files/styles/fixed-width-778/public/web-jeans_for_jesus_011.jpg.jpg?itok=V9JCaijI',
+        'http://d8mlrvfocjtax.cloudfront.net/artists/3979_36612_original.jpg?1485707709',
 
       ],
       show: false
@@ -190,14 +205,25 @@ export default {
   background-color: none;
 }
 #section2 {
-  height: 100vh;
+  height: 90vh;
   background-color: none;
 }
 #section3 {
-  height: 100vh;
-  background-color: #ffffff;
+  iframe {
+    margin-left: 10vw;
+    height: 20vw;
+    width: 35vw;
+    -webkit-filter: grayscale(100%);
+    filter: grayscale(100%);
+    display: inline-block;
+  }
 }
 #section4 {
-  height: 100vh;
+  text-align: center;
+  padding-left: 20vw;
+  padding-right: 20vw;
+  color: #FF9A56;
+  margin-top: 100px;
+  margin-bottom: 100px;
 }
 </style>
